cd666aab7c977d56ac9c6af62a2444a9ff448023
MateoB <mateobassani@hotmail.com>
2021-01-10 20:24:23 -0300

    Changed subscribe btn color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'green-btn': '#25B350',
+      'green-btn-hover': '#187333'
+     }),
     fontFamily: {
       'sans': ['Raleway', 'Sans-serif'],
       'sans2': ['Work Sans', 'Sans-serif'],
