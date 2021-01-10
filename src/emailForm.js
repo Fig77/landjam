e68754f043c49e4cf8a607418b9ab45e58ca2d7d
fig77 <fig.igle@gmail.com>
2021-01-10 15:39:53 -0300

    Handingl onChange func

--- src/emailForm.js
+++ src/emailForm.js
@@ -8,13 +8,17 @@ const EmailForm = () => {
     setMailVar('');
   }
 
+  const setVar = e => {
+   setMailVar(e.target.value)
+  }
+
  return (
    <form className='class="mt-8 space-y-6"' action="https://jamform.com/f/PEBD4HJHAvR2ZQWrcNj7a" method="POST" target='#'>
       <h2 class="mt-6 text-center text-2xl font-extrabold text-gray-900">
         Mantente Informado
       </h2>
      <label for="email-address" class="sr-only">Email</label>
-     <input  value = {() => { emailVar(); }} id="email-address" name="email" type="email" required className="pt-4 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+     <input  value = {() => { emailVar(); }} onChange = { () => { setVar(e)} }   id="email-address" name="email" type="email" required className="pt-4 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
      <div className='pt-4'>
        <button  onclick = { () => {  clearMail(); }} type="submit" class="group relative w-full flex justify-center py-1 px-2  border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
