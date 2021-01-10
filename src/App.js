27f4071e7024180d281cde79e9031ece857a24bd
MateoB <mateobassani@hotmail.com>
2021-01-10 19:38:54 -0300

    Landing page finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import logo from './logo.svg';
+import logo from './assets/img/logo.png';
+import logo2 from './assets/img/logo2.png';
+import fb from './assets/img/fb.svg';
+import ig from './assets/img/ig.svg';
+import wp from './assets/img/wp.svg';
+import mainbg from './assets/img/mainbg.png';
+// import mainbg from './assets/img/mainbg2.jpg';
 import './App.css';
 import EmailForm from './emailForm';
 
 function App() {
   return (
-   <main className="min-h-screen flex flex-col items-center justify-between items-center bg-gray-50">
-   <header class="bg-gray-800 w-full p-4 flex items-start  self-start">
-     <h1 className='self-start text-white'>Logo placeholder</h1>
+   <main className="min-h-screen flex flex-col items-center justify-between items-center bg-gray-50" style={{background:`linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${mainbg})`}}>
+   <header className="black-bar top-bar  w-full p-4 flex items-start  self-start">
+    <img src={logo} alt="Logo" className="logo-nav"/>
    </header>
-      <div className='px-4 py-12'>
-	<EmailForm />
+      <div className='flex flex-col justifiy-between items-center self-center text-center'>
+        <h1 className="font-sans2 font-black text-white tracking-widest phones-pt:text-3xl mt-2 mb-6 px-1 phones: text-5xl mt-5 desktop: text-4xl">-PRÓXIMAMENTE-</h1>
+        <div className="main-subtext">
+          <h2 className="font-sans text-white tracking-wider phones-pt:text-lg mb-3 px-2 phones: mx-2 desktop: text-lg">TE INVITAMOS A DISFRUTAR DE UNA MANERA MÁS SENCILLA DE ELEGIR EL MOMENTO Y EL LUGAR EN DONDE QUIERAS IR DE LA MANERA QUE SOLO NOSOTROS PODEMOS BRINDARTE</h2>
+        </div>
+        <h3 className="text-white text-sm font-light tracking-ultra phones-pt:mb-3 phones:mb-4 desktop: text-base mb-4">
+        Obtené descuentos exclusivos y <br/> enterate de las novedades de
+        </h3>
+        <img src={logo2} alt="Logo main" className="logo-main phones-pt:mb-3 phones:mb-4 desktop: mb-4"/>
+        <EmailForm />
+        <div className="flex justify-center items-center">
+          <a href="#"><img src={fb} className="social-button"/></a>
+          <a href="#"><img src={ig} className="social-button"/></a>
+          <a href="#"><img src={wp} className="social-button"/></a>
+        </div>
       </div>
-     <footer className="bg-gray-800 w-full px-2 pt-2 pb-2 flex justify-between items-center">
-       <span>footer</span>
-       <span>social media </span>
+     <footer className="black-bar w-full px-2 pt-2 pb-2 flex justify-center items-center text-white mb-1">
+       <span>Copyright 	&copy; HandyRess. Todos los derechos reservados.</span>
      </footer>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Raleway', 'Sans-serif'],
+      'sans2': ['Work Sans', 'Sans-serif'],
+    },
+    letterSpacing: {
+      ultra: '0.2em',
+     },
+     screens: {
+      'phones-pt': {'raw': 'only screen and (max-width: 767px) and (orientation: portrait)'},
+      'phones': {'raw': 'only screen and (max-width: 767px)'},
+      'desktop': {'raw': 'only screen and (min-width: 768px)'}
+    },
     extend: {},
   },
   variants: {
