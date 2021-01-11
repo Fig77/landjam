e4466d42e3390ab4706dd63c9770ca1279545698
fig77 <fig.igle@gmail.com>
2021-01-10 23:20:03 -0300

    Fixed script on json

--- src/emailForm.js
+++ src/emailForm.js
@@ -48,7 +48,7 @@ return (
      <label for="email-address" class="sr-only">Email</label>
      <input  value = {  emailVar } onChange = { e => setVar(e) } id="email-address" name="email" type="email" required className="pt-4 px-6 py-4 appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-full focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="E-mail" />
      <div className='pt-4 flex justify-center items-center'>
-     <button type="submit" className={ `${disabled ? 'load' : 'bg-green-btn' } group relative flex justify-center py-3 px-5 border border-transparent text-sm font-medium rounded-full text-white  hover:bg-green-btn-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}>
+     <button type="submit" className={ `${disabled ? 'load' : 'bg-green-btn' } group relative flex justify-center py-3 px-5 border border-transparent text-sm font-medium rounded-full text-white  hover:bg-green-btn-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`} disabled={disabled}>
         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
           <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
         </span>
